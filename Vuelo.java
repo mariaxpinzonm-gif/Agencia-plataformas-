ec65d4a0c0c07d94241d94dba4f241c67bb8a4b6
Angelina Correa López <angelina.correal@autonoma.edu.co>
2026-03-17 19:09:54 -0500

    Soy Angelina y Actualice Vuelo.java agegando el método de ConfirmarReserva

--- Vuelo.java
+++ Vuelo.java
@@ -1,4 +1,40 @@
-public class Vuelo{
+public class Vuelo {
 
-  
+    String numeroVuelo;
+    String aerolinea;
+    String destino;
+    String fecha;
+
+    String[] destinosPermitidos = {"Cartagena", "Santa Marta", "Cali"};
+
+    public Vuelo(String numeroVuelo, String aerolinea, String destino, String fecha) {
+
+        this.numeroVuelo = numeroVuelo;
+        this.aerolinea = aerolinea;
+        this.fecha = fecha;
+
+        boolean valido = false;
+
+        for (String d : destinosPermitidos) {
+            if (d.equals(destino)) {
+                valido = true;
+            }
+        }
+
+        if (valido) {
+            this.destino = destino;
+        } else {
+            this.destino = "No válido";
+        }
+    }
+
+    public boolean confirmarReserva() {
+
+        if (destino.equals("No válido")) {
+            System.out.println("Destino del vuelo no permitido");
+            return false;
+        }
+
+        return true;
+    }
 }
