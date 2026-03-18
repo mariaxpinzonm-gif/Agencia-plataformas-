1bb52bf7940445a1b99d15e3b6628d8665f91b60
mariaxpinzonm-gif <mariax.pinzonm@autonoma.edu.co>
2026-03-18 01:07:24 +0000

    Añadida mejora en la visualización de la reserva

--- Reserva.java
+++ Reserva.java
@@ -1,5 +1,35 @@
-public class Reserva{
 
 
-  
+public class Reserva {
+    private Cliente cliente;
+    private Vuelo vuelo;
+    private Hotel hotel;
+    private String fechaReserva;
+
+    public Reserva(Cliente cliente, Vuelo vuelo, Hotel hotel, String fechaReserva) {
+        this.cliente = cliente;
+        this.vuelo = vuelo;
+        this.hotel = hotel;
+        this.fechaReserva = fechaReserva;
+    }
+
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+
+    public Vuelo getVuelo() { return vuelo; }
+    public void setVuelo(Vuelo vuelo) { this.vuelo = vuelo; }
+
+    public Hotel getHotel() { return hotel; }
+    public void setHotel(Hotel hotel) { this.hotel = hotel; }
+
+    public String getFechaReserva() { return fechaReserva; }
+    public void setFechaReserva(String fechaReserva) { this.fechaReserva = fechaReserva; }
+
+    public void mostrarResumen() {
+        System.out.println(cliente.getNombreCompleto());
+        System.out.println(vuelo.getAerolinea() + " " + vuelo.getClaseReserva());
+        System.out.println(hotel.getNombre() + " " + hotel.getRegimen());
+        System.out.println(fechaReserva);
+    }
 }
+  
